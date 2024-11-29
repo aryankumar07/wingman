79d93c2118a446dbeb45068024163f872fff83bc
aryankumar07 <ak2839426@gmail.com>
2024-11-29 21:14:55 +0530

    fix: deploy bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import axios from 'axios';
 import './App.css'
 import NavBar from './components/navbar/navbar';
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Cart from './pages/cart';
 
 function App() {
-
-
-  const fetchreq = async ()=>{
-    try{
-      const res = await axios.get("https://fakestoreapi.com/products");
-      console.log(res)
-    }catch(e){
-      console.log(e)
-    }
-  }
-
-
   return (
     <div>
       <NavBar/>
